929ff64328cc23a5795942153500ec7de9456d22
Maddy <mmcintosh2k14@gmail.com>
2019-12-15 23:48:07 -0500

    adds hero image and hire me button

---
+++ portfolio/src/components/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Hero = () => {
+    return (
+     
+            
+           
+            
+            <div class="hero-text">
+            <h1 >
+                I am Madeline McIntosh
+            </h1>
+            <p>Lambda School student and aspiring Web Developer</p>
+            <button>Hire me</button>
+            </div>
+      
+    )
+}
+
+export default Hero;
